cda98dba7b0c466b3d790a4c2be5631f3d48380d
HiiWee <wpdlzhf159@gmail.com>
2023-11-06 14:43:34 +0900

    style: 인덴트 정리

--- src/test/java/lotto/domain/lotto/LottoRewardConditionTest.java
+++ src/test/java/lotto/domain/lotto/LottoRewardConditionTest.java
@@ -1,9 +1,8 @@
 package lotto.domain.lotto;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
