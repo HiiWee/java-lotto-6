ff753c454c6986b28acd9cefaeb0e4207d910f10
HiiWee <wpdlzhf159@gmail.com>
2023-11-09 21:24:57 +0900

    refactor: boolean 플래그 추가해 당첨 결과 서치 로직 간소화

--- src/main/java/lotto/domain/lotto/LottoRewardCondition.java
+++ src/main/java/lotto/domain/lotto/LottoRewardCondition.java
@@ -4,18 +4,20 @@ import java.util.Arrays;
 
 public enum LottoRewardCondition {
 
-    FAIL(0, 0),
-    FIFTH_WINNER(3, 5_000L),
-    FOURTH_WINNER(4, 50_000L),
-    THIRD_WINNER(5, 1_500_000L),
-    SECOND_WINNER(5, 30_000_000L),
-    FIRST_WINNER(6, 2_000_000_000L);
+    FAIL(0, false, 0),
+    FIFTH_WINNER(3, false, 5_000L),
+    FOURTH_WINNER(4, false, 50_000L),
+    THIRD_WINNER(5, false, 1_500_000L),
+    SECOND_WINNER(5, true, 30_000_000L),
+    FIRST_WINNER(6, false, 2_000_000_000L);
 
     private final int winningCount;
+    private final boolean hasBonusNumber;
     private final long rewardMoney;
 
-    LottoRewardCondition(final int winningCount, final long rewardMoney) {
+    LottoRewardCondition(final int winningCount, final boolean hasBonusNumber, final long rewardMoney) {
         this.winningCount = winningCount;
+        this.hasBonusNumber = hasBonusNumber;
         this.rewardMoney = rewardMoney;
     }
 
@@ -28,24 +30,13 @@ public enum LottoRewardCondition {
     }
 
     public static LottoRewardCondition findLottoReward(final int winningCount, final boolean hasBonusNumber) {
-        if (isSecondWinning(winningCount, hasBonusNumber)) {
-            return SECOND_WINNER;
-        }
-        return compareWithoutBonus(winningCount);
-    }
-
-    private static boolean isSecondWinning(final int winningCount, final boolean hasBonusNumber) {
-        return hasBonusNumber && winningCount == SECOND_WINNER.winningCount;
-    }
-
-    private static LottoRewardCondition compareWithoutBonus(final int winningCount) {
         return Arrays.stream(values())
-                .filter(reward -> reward.isSameWinningCount(winningCount) && reward != SECOND_WINNER)
+                .filter(reward -> reward.isSameCondition(winningCount, hasBonusNumber))
                 .findAny()
                 .orElse(FAIL);
     }
 
-    private boolean isSameWinningCount(final int winningCount) {
-        return winningCount == this.winningCount;
+    private boolean isSameCondition(final int winningCount, final boolean hasBonusNumber) {
+        return this.winningCount == winningCount && this.hasBonusNumber == hasBonusNumber;
     }
 }
